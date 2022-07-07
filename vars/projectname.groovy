152f75797d1a56468023ed8dff612982e8a7044f
k.mitarai <65850380+nyangame@users.noreply.github.com>
2022-07-07 13:55:45 +0900

    Create projectname.groovy

---
+++ vars/projectname.groovy
@@ -0,0 +1,14 @@
+def get_project_uri(uri) { 
+    def projectUri = uri.replaceFirst(/https\:\/\//,"")
+    projectUri = projectUri.replaceFirst(/http\:\/\//,"")
+    projectUri = projectUri.replaceFirst(/git\@\/\//,"")
+    projectUri = projectUri.replaceFirst(/www\./,"")
+    projectUri = projectUri.replaceFirst(/github\.com\//,"")
+    projectUri = projectUri.replaceFirst(/github\.com\:/,"")
+    projectUri = projectUri.replaceFirst(/MELPOT\//,"")
+    return projectUri;
+}
+
+def get_project_path(projectUri) {
+    return env.WORKSPACE + ".\\..\\" + projectUri;
+}
